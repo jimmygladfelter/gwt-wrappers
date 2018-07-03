5c857d998ade7542439c6c33db6a69e714c6e0e8
James C. Gladfelter <James C. Gladfelter@jgladfelter-pc>
2018-07-02 21:02:19 -0400

    Implementing CreateJs

--- createjs-gwtwrapper/src/com/gwt/createjs/easeljs/Event.java
+++ createjs-gwtwrapper/src/com/gwt/createjs/easeljs/Event.java
@@ -16,20 +16,56 @@ public class Event extends EventProperties {
 
 	protected Event() {
 	}
-	
+
+	/**
+	 * Returns a clone of the Event instance.
+	 */
 	public final native Event clone() /*-{
 		return this.clone();
 	}-*/;
-	
+
+	/**
+	 * Sets defaultPrevented to true if the event is cancelable. Mirrors the DOM
+	 * level 2 event standard. In general, cancelable events that have
+	 * preventDefault() called will cancel the default behaviour associated with the
+	 * event.
+	 */
 	public final native void preventDefault() /*-{
 		this.preventDefault();
 	}-*/;
-	
+
+	/**
+	 * Causes the active listener to be removed via removeEventListener();
+	 */
 	public final native void remove() /*-{
 		this.remove();
 	}-*/;
-	
+
+	/**
+	 * Provides a chainable shortcut method for setting a number of properties on
+	 * the instance.
+	 * 
+	 * @param jso
+	 *            - a generic object containing properties to copy to the instance.
+	 * @return returns the instance the method is called on (useful for chaining
+	 *         calls.)
+	 */
 	public final native Event set(JavaScriptObject jso) /*-{
-		return this.set();
+		return this.set(jso);
+	}-*/;
+
+	/**
+	 * Sets propagationStopped and immediatePropagationStopped to true. Mirrors the
+	 * DOM event standard.
+	 */
+	public final native void stopImmediatePropagation() /*-{
+		this.stopImmediatePropagation();
+	}-*/;
+
+	/**
+	 * Sets propagationStopped to true. Mirrors the DOM event standard.
+	 */
+	public final native void stopPropagation() /*-{
+		this.stopPropagation();
 	}-*/;
 }
